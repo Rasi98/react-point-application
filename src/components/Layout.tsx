0ba8bddf674ba5d5182a7ef6b633e1da1f67cdcf
Lakshan Rasingolla <rwmlakshan@gmail.com>
2022-05-17 21:39:29 +0530

    application commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,74 @@
+import React, {useState} from "react";
+import Grid from "@mui/material/Grid";
+
+import Team from "./Team";
+import {ITeam} from "../types/team";
+
+
+const Layout: React.FC = () => {
+    const teamList=[
+        {id:1,name:'A Team',points:0},
+        {id:2,name:'Pied-piper',points:0},
+        {id:3,name:'Noob pirates',points:0},
+        {id:4,name:'Team D',points:0},
+        {id:5,name:'Team E',points:0}];
+
+    const [team,setTeam] = useState<ITeam[]>(teamList);
+
+    const handleAddPoint= (name:string,point:number,id:number) => {
+        console.log("p:",point,"id:",id);
+        const teamList: ITeam[] = team.slice();
+        const index = teamList.findIndex((team: ITeam) => team.id === id);
+        teamList.splice(index,1,{name:name,points:point,id:id});
+        setTeam(teamList);
+    }
+
+    const handleSubPoint= (name:string,point:number,id:number) => {
+        console.log("p:",point,"id:",id);
+        const teamList: ITeam[] = team.slice();
+        const index = teamList.findIndex((team: ITeam) => team.id === id);
+        teamList.splice(index,1,{name:name,points:point,id:id});
+        setTeam(teamList);
+    }
+
+    const renderTeams= () => {
+        if (!team){
+            return;
+        }
+        return team.map((team:ITeam) =>
+        <Team
+            team={team}
+            handleAddPoint={handleAddPoint}
+            handleSubPoint={handleSubPoint}
+            key={team.id}/>
+        )
+    }
+
+    return(
+        <Grid container>
+            <Grid
+                container
+                justifyContent={'center'}
+            >
+                <Grid
+                    item
+                    style={{
+                        marginBottom:'2%'
+                    }}
+                >
+                    <h1>POINT TABLE</h1>
+                </Grid>
+            </Grid>
+             <Grid
+                 container
+                 justifyContent="space-evenly"
+                 columnSpacing={3}
+                 rowSpacing={3}
+                 >
+                 {renderTeams()}
+             </Grid>
+        </Grid>
+
+    )
+}
+export default Layout;
---
+++ src/components/Team.tsx
@@ -0,0 +1,112 @@
+import React, {FormEvent, useState} from "react";
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import InputAdornment from '@mui/material/InputAdornment';
+import {Button, Paper} from "@mui/material";
+
+import {ITeam} from "../types/team";
+
+type TeamProps={
+    team: ITeam;
+    handleAddPoint: (name:string,point:number,id:number) => void;
+    handleSubPoint: (name:string,point:number,id:number) => void;
+}
+const Team: React.FC <TeamProps>= (props:TeamProps) => {
+    const [point,setPoint] = useState<number | null>(null);
+    const [error,setError] = useState(false);
+    const{name,points,id} = props.team;
+
+    const handleAdd= (e:FormEvent) => {
+        e.preventDefault();
+        if (!point){
+            setError(true);
+            return;
+        }
+        let newPoint= points+point;
+        props.handleAddPoint(name,newPoint,id);
+        setPoint(null);
+    }
+
+    const handleSub= (e:FormEvent) => {
+        e.preventDefault();
+        if (!point){
+            setError(true)
+            return;
+        }
+        let newPoint= points-point;
+        props.handleSubPoint(name,newPoint,id);
+        setPoint(null);
+    }
+
+    return(
+        <Grid
+            item
+            xs={12} sm={6} md={4}
+        >
+            <Paper
+                elevation={5}
+                style={{
+                    paddingTop: '5%',
+                    paddingBottom: '8%',
+                    borderRadius: 10
+                }}
+            >
+                <Grid
+                    container
+                    direction='column'
+                    alignItems="center"
+                >
+                    <h2>{name}</h2>
+                    <h4>${points}</h4>
+                    <TextField
+                        error={error}
+                        id={error ? "outlined-error-helper-text" : "outlined-basic"}
+                        helperText ={error ? "Cannot be empty!" : null}
+                        onFocus={() => setError(false)}
+                        label={"Points"}
+                        type="number"
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                        }}
+                        size={'small'}
+                        value={point ? point : ''}
+                        style={{
+                            marginTop:'2%',
+                            marginBottom:'2%'
+                        }}
+                        variant="outlined"
+                        onChange={(e)=>setPoint(parseFloat(e.target.value))}
+                    />
+                    <Grid
+                        container
+                        xs={6}
+                        alignItems="center"
+                        justifyContent="space-evenly"
+                        >
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="error"
+                                startIcon={<RemoveCircleOutlineIcon/>}
+                                size="small"
+                                onClick={handleSub}
+                            >Sub</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="success"
+                                startIcon={<AddCircleOutlineIcon/>}
+                                size="small"
+                                onClick={handleAdd}
+                            >Add</Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Grid>
+    )
+}
+export default Team;
